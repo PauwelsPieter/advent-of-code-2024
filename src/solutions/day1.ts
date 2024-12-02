b020273b4e6f27be3e9b6ade3fffdca69a0d1940
Pieter <pieter.pauwels@appwise.be>
2024-12-02 01:35:25 +0100

    Day 1, Puzzle 2

--- src/solutions/day1.ts
+++ src/solutions/day1.ts
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs'
 import * as readline from 'node:readline'
 
+// Time complexity: n*log(n)
 export async function puzzle1() {
 	const fileStream = fs.createReadStream('./input/day1.txt')
 	const rl = readline.createInterface({
@@ -35,6 +36,40 @@ export async function puzzle1() {
 	})
 }
 
+// Time complexity: n
 export async function puzzle2() {
-	console.log('Running puzzle 2!')
+	const fileStream = fs.createReadStream('./input/day1.txt')
+	const rl = readline.createInterface({
+		input: fileStream,
+		output: process.stdout,
+		terminal: false,
+	})
+
+	const leftList: number[] = []
+	const rightList: number[] = []
+
+	rl.on('line', (line) => {
+		const [first, second] = line.split(/\s+/)
+
+		leftList.push(Number(first))
+		rightList.push(Number(second))
+	})
+
+	rl.on('close', () => {
+		const rightListCountMap = new Map<number, number>()
+
+		for (const number of rightList) {
+			rightListCountMap.set(number, (rightListCountMap.get(number) ?? 0) + 1)
+		}
+
+		let totalSimilarityScore = 0
+
+		for (const number of leftList) {
+			const similarityScore = number * (rightListCountMap.get(number) ?? 0)
+
+			totalSimilarityScore += similarityScore
+		}
+
+		console.log(`Total similarity score: ${totalSimilarityScore}`)
+	})
 }
