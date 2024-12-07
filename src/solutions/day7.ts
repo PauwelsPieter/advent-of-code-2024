6d8fabb19f4dd443e9c9a271e766ba340aaf7661
Pieter <pieter.pauwels@appwise.be>
2024-12-07 10:16:25 +0100

    Day 7, Puzzle 2

--- src/solutions/day7.ts
+++ src/solutions/day7.ts
@@ -28,8 +28,32 @@ export async function puzzle1() {
 	})
 }
 
-// Time complexity:
-export async function puzzle2() {}
+// Time complexity: O(3^n)
+export async function puzzle2() {
+	const fileStream = fs.createReadStream('./input/day7.txt')
+	const rl = readline.createInterface({
+		input: fileStream,
+		output: process.stdout,
+		terminal: false,
+	})
+
+	let correctEquationTotal = 0
+
+	rl.on('line', (line) => {
+		const [result, valuesString] = line.split(':')
+		const values = valuesString.trim().split(' ').map(Number)
+
+		if (isCorrectEquation2(Number(result), values)) {
+			correctEquationTotal += Number(result)
+		}
+	})
+
+	rl.on('close', () => {
+		console.log(
+			`Total calibration result of correct equations: ${correctEquationTotal}`,
+		)
+	})
+}
 
 function isCorrectEquation(result: number, values: number[]): boolean {
 	const addResult = values[0] + values[1]
@@ -42,5 +66,21 @@ function isCorrectEquation(result: number, values: number[]): boolean {
 	return (
 		isCorrectEquation(result, [addResult, ...values.slice(2)]) ||
 		isCorrectEquation(result, [multiplyResult, ...values.slice(2)])
+	)
+}
+
+function isCorrectEquation2(result: number, values: number[]): boolean {
+	const addResult = values[0] + values[1]
+	const multiplyResult = values[0] * values[1]
+	const concatResult = Number(values[0].toString()+values[1].toString())
+
+	if (values.length === 2) {
+		return addResult === result || multiplyResult === result || concatResult == result
+	}
+
+	return (
+		isCorrectEquation2(result, [addResult, ...values.slice(2)]) ||
+		isCorrectEquation2(result, [multiplyResult, ...values.slice(2)]) ||
+		isCorrectEquation2(result, [concatResult, ...values.slice(2)])
 	)
 }
