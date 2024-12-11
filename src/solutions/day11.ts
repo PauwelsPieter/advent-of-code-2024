abde82c7c2f5e2ab81295901a4d336fd1191c5a2
Pieter <pieter.pauwels@appwise.be>
2024-12-11 19:47:26 +0100

    Day 11, Puzzle 2

--- src/solutions/day11.ts
+++ src/solutions/day11.ts
@@ -2,39 +2,116 @@ import * as fs from 'node:fs'
 
 // Time complexity: O(n*2^24)
 export async function puzzle1() {
-	let stones = fs
+	const stones = fs
 		.readFileSync('./input/day11.txt', { encoding: 'utf-8' })
 		.split(' ')
 		.map(Number)
 
+	let countMap = new Map<number, number>()
+
+	for (const stone of stones) {
+		countMap.set(stone, (countMap.get(stone) ?? 0) + 1)
+	}
+
 	for (let i = 0; i < 25; i++) {
-		const stonesAfterBlink: number[] = []
-		for (let j = 0; j < stones.length; j++) {
-			const stone = stones[j]
-
-			if (stone === 0) {
-				stonesAfterBlink.push(1)
-				continue
-			}
-
-			const digitLength = stone.toString().length
-			if (digitLength % 2 === 0) {
-				stonesAfterBlink.push(
-					...[
-						Number(stone.toString().substring(0, digitLength / 2)),
-						Number(stone.toString().substring(digitLength / 2)),
-					],
+		const countMapAfterBlink = new Map<number, number>()
+		for (const [number, count] of countMap) {
+			if (number === 0) {
+				const newNumber = 1
+				countMapAfterBlink.set(
+					newNumber,
+					(countMapAfterBlink.get(newNumber) ?? 0) + count,
 				)
 				continue
 			}
 
-			stonesAfterBlink.push(stone * 2024)
+			const numberString = number.toString()
+			const digitLength = numberString.length
+			if (digitLength % 2 === 0) {
+				const newNumber1 = Number(numberString.substring(0, digitLength / 2))
+				const newNumber2 = Number(numberString.substring(digitLength / 2))
+				countMapAfterBlink.set(
+					newNumber1,
+					(countMapAfterBlink.get(newNumber1) ?? 0) + count,
+				)
+				countMapAfterBlink.set(
+					newNumber2,
+					(countMapAfterBlink.get(newNumber2) ?? 0) + count,
+				)
+				continue
+			}
+
+			const newNumber = number * 2024
+			countMapAfterBlink.set(
+				newNumber,
+				(countMapAfterBlink.get(newNumber) ?? 0) + count,
+			)
 		}
-		stones = stonesAfterBlink
+		countMap = countMapAfterBlink
 	}
 
-	console.log(`Amount of stones: ${stones.length}`)
+	let stoneCount = 0
+	for (const [_number, count] of countMap) {
+		stoneCount += count
+	}
+
+	console.log(`Amount of stones: ${stoneCount}`)
 }
 
-// Time complexity:
-export async function puzzle2() {}
+// Time complexity: O(n*2^74)
+export async function puzzle2() {
+	const stones = fs
+		.readFileSync('./input/day11.txt', { encoding: 'utf-8' })
+		.split(' ')
+		.map(Number)
+
+	let countMap = new Map<number, number>()
+
+	for (const stone of stones) {
+		countMap.set(stone, (countMap.get(stone) ?? 0) + 1)
+	}
+
+	for (let i = 0; i < 75; i++) {
+		const countMapAfterBlink = new Map<number, number>()
+		for (const [number, count] of countMap) {
+			if (number === 0) {
+				const newNumber = 1
+				countMapAfterBlink.set(
+					newNumber,
+					(countMapAfterBlink.get(newNumber) ?? 0) + count,
+				)
+				continue
+			}
+
+			const numberString = number.toString()
+			const digitLength = numberString.length
+			if (digitLength % 2 === 0) {
+				const newNumber1 = Number(numberString.substring(0, digitLength / 2))
+				const newNumber2 = Number(numberString.substring(digitLength / 2))
+				countMapAfterBlink.set(
+					newNumber1,
+					(countMapAfterBlink.get(newNumber1) ?? 0) + count,
+				)
+				countMapAfterBlink.set(
+					newNumber2,
+					(countMapAfterBlink.get(newNumber2) ?? 0) + count,
+				)
+				continue
+			}
+
+			const newNumber = number * 2024
+			countMapAfterBlink.set(
+				newNumber,
+				(countMapAfterBlink.get(newNumber) ?? 0) + count,
+			)
+		}
+		countMap = countMapAfterBlink
+	}
+
+	let stoneCount = 0
+	for (const [_number, count] of countMap) {
+		stoneCount += count
+	}
+
+	console.log(`Amount of stones: ${stoneCount}`)
+}
