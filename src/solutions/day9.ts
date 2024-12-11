5e4d2fa3efa4393185ce8410826a982b880b126c
Pieter <pieter.pauwels@appwise.be>
2024-12-11 13:52:00 +0100

    Day 9, Puzzle 2

--- src/solutions/day9.ts
+++ src/solutions/day9.ts
@@ -37,8 +37,70 @@ export async function puzzle1() {
 		p1 += 1
 	}
 
-	console.log(checksum)
+	console.log(`Resulting filesystem checksum: ${checksum}`)
 }
 
-// Time complexity:
-export async function puzzle2() {}
+// Time complexity: O(n^2)
+export async function puzzle2() {
+	const diskMap = fs
+		.readFileSync('./input/day9.txt', { encoding: 'utf-8' })
+		.split('')
+		.map(Number)
+	const fileSystem: Array<{ fileId: number | null; blocks: number }> = []
+
+	for (let i = 0; i < diskMap.length; i++) {
+		if (i % 2 === 0) {
+			fileSystem.push({
+				fileId: i / 2,
+				blocks: diskMap[i],
+			})
+		} else {
+			fileSystem.push({
+				fileId: null,
+				blocks: diskMap[i],
+			})
+		}
+	}
+
+	let checksum = 0
+	let position = 0
+
+	while (fileSystem.length > 0) {
+		const current = fileSystem.shift()!
+
+		if (typeof current.fileId === 'number') {
+			for (let i = 0; i < current.blocks; i++) {
+				checksum += position * (current.fileId as number)
+				position += 1
+			}
+		} else {
+			// We've free space
+			let freeSpace = current.blocks
+
+			// Loop backwards over the filesystem
+			for (let i = fileSystem.length - 1; i >= 0; i--) {
+				// Ignore empty blocks
+				if (typeof fileSystem[i].fileId === 'number') {
+					if (fileSystem[i].blocks <= freeSpace) {
+						for (let j = 0; j < fileSystem[i].blocks; j++) {
+							checksum += position * (fileSystem[i].fileId as number)
+							position += 1
+						}
+						freeSpace -= fileSystem[i].blocks
+						// Set the moved space to free space
+						fileSystem[i].fileId = null
+						if (freeSpace === 0) {
+							break
+						}
+					}
+				}
+			}
+
+			if (freeSpace > 0) {
+				position += freeSpace
+			}
+		}
+	}
+
+	console.log(`Resulting filesystem checksum: ${checksum}`)
+}
